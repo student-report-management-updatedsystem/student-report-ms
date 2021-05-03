c7484cd7239c0c85df40d03aaf0b42994ccd14e5
aryauresh <66162119+aryauresh@users.noreply.github.com>
2021-05-03 20:55:14 +0530

    created new dashboard in 'panel.php'

--- panel.php
+++ panel.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
  if(!isset($_SESSION['adminloginid'])){
-   header("location: admin_login.php");
+   header("location: index.php");
  }
 ?>
 <!DOCTYPE html>
@@ -9,32 +9,94 @@
   <head>
     <meta charset="utf-8">
     <title>Panel Page</title>
-    <link rel="stylesheet" href="main/css/bootstrap-responsive.css">
-    <style>
-
-      body{
-        margin: o;
-        padding: 0;
-      }
-      div.header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 60px;
-        background-image: linear-gradient(to right,#2a2a72, #009ffd);
-      }
-      div.header button{
-        padding: 10px;
-        margin: 15px;
-      }
-    </style>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="panel.css">
   </head>
   <body>
-     <div class="header">
-       <h2>Welcome to ABC panel - <?php echo($_SESSION['adminloginid']) ?></h2>
-       <form class="" method="POST">
-         <a href="index.php"><button type="button" name="logout">LOG OUT</button></a>
-       </form>
-     </div>
+
+      <div class="sidebar">
+        <div class="sidebar-brand">
+          <h2><span class="lab la-accusoft"></span> ABC School </h2>
+        </div>
+
+        <div class="sidebar-menu">
+          <ul>
+            <li>
+              <a href=""><span class="las la-school"></span>
+                <span>Dashboard</span></a>
+            </li>
+            <li>
+              <a href=""><span class="las la-user-graduate"></span>
+                <span>View</span></a>
+            </li>
+            <li>
+              <a href=""><span class="las la-graduation-cap"></span>
+                <span>Add</span></a>
+            </li>
+            <li>
+              <a href=""><span class="las la-user-minus"></span>
+                <span>Delete</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="main-content">
+         <header>
+           <h2>
+             <label for="">
+               <span class="las la-bars"></span>
+             </label>
+
+             Dashboard
+           </h2>
+
+           <div class="search-wrapper">
+             <span class="las la-search"></span>
+             <input type="search" placeholder="Search Here" value="">
+           </div>
+
+           <div class="user-wrapper">
+             <img src="https://img.icons8.com/pastel-glyph/64/000000/user-male--v1.png" width="40px" height="40px"/>
+             <div>
+               <h4>John Doe</h4>
+               <small>Super Admin</small>
+             </div>
+           </div>
+         </header>
+
+         <main>
+            <div class="cards">
+              <div class="card-single">
+                <div>
+                  <h1>54</h1>
+                  <span>Students</span>
+                </div>
+                <div>
+                  <span class="las la-users"></span>
+                </div>
+              </div>
+              <div class="card-single">
+                <div>
+                  <h1>79</h1>
+                  <span>Add Students</span>
+                </div>
+                <div>
+                  <span class="las la-graduation-cap"></span>
+                </div>
+              </div>
+              <div class="card-single">
+                <div>
+                  <h1>124</h1>
+                  <span>Delete Students</span>
+                </div>
+                <div>
+                  <span class="las la-user-minus"></span>
+                </div>
+              </div>
+            </div>
+         </main>
+      </div>
+
   </body>
 </html>
