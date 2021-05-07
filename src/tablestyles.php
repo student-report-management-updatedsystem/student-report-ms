9fc44c6b85e75fb2edc302062f3853ef1e426f80
E Anjali Sankar <anjuqz0@gmail.com>
2021-05-07 20:59:04 +0530

    view student table

---
+++ src/tablestyles.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+  *{margin: 0;padding: 0;box-sizing :border-box; font-family:'arial'};
+
+  .main-div
+  {
+    width:100%;
+    height:100vh;
+    display :flex;
+    flex-direction: :column;
+    justify-content:center;
+    align-items:center;
+  }
+
+  .center-div
+  {
+    width:90%;
+    height:80vh;
+    background: -webkit-linear-gradient(lest,#5DADE2,#00c6ff);
+    padding: 20px 0 0 0;
+    box-shadow: 0 10px 20px 0 rgba(0,0,0,.03);
+    border-radius:10px;
+
+  }
+
+  h1
+  {
+    font-size:18px;
+    color:#000;
+    text-align: center;
+    margin-top: -20px;
+    margin-bottom: 20px;
+  }
+
+  table
+  {
+    border-collapse:collapse;
+    background-color:#fff;
+    box-shadow: 0 10px 20px 0 rgba(0,0,0,.03);
+    border-radius: 10px;
+    margin : auto;
+  }
+
+  th,td
+  {
+    border:1px solid #f2f2f2;
+    padding: 8px 30px;
+    text-align: center;
+    color: grey;
+  }
+  th
+  {
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+ td
+ {
+   font-size: 13px;
+ }
+ .post-class{
+   text-transform:uppercase;
+ }
+ .fa{
+   font-size:18px
+ }
+ .fa-edit
+ {
+   color:#63c76a;
+ }
+ .fa-trash
+ {
+   color:#ff0000;
+ }
+ a{
+   text-decoration: none;
+   display:flex;
+   justify-content: center;
+ }
+</style>
