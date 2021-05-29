9096315b3fee6d9f7ddad230b6909da8bdf17ae4
anjusuresh1 <anjusuresh2001@gmail.com>
2021-05-29 18:36:22 +0400

    user database

--- database/miniproject.sql
+++ database/miniproject.sql
@@ -1,18 +1,15 @@
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(55) NOT NULL,
-  `position` varchar(55) NOT NULL,
   `username` varchar(55) NOT NULL,
   `password` varchar(55) NOT NULL
 
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`id`, `name`, `position`, `username`, `password`) VALUES
-(1, 'AS', 'admin', 'admin', '123');
+INSERT INTO `user` (`username`, `password`) VALUES
+('admin', '123');
 
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`username`);
 
 CREATE TABLE IF NOT EXISTS `student` (
   `id` int(11) NOT NULL UNIQUE PRIMARY KEY,
@@ -29,10 +26,6 @@ CREATE TABLE IF NOT EXISTS `student` (
 
 INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `report`, `yoa`, `parent`, `dob`, `gender`, `file`) VALUES
 (1, 'AS01-2341', 'Ron', 'Weasley', 'hard-working', '2012', '08138652645', '1993-05-05', 'male', 'ronweasley.jpg');
-
-
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `student`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
