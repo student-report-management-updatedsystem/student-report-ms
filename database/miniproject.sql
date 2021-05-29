9ed4e925a624711a7ce6fc44b1bc31ea1697e6d6
anjusuresh1 <anjusuresh2001@gmail.com>
2021-05-29 22:07:27 +0400

    cgp database

--- database/miniproject.sql
+++ database/miniproject.sql
@@ -15,17 +15,18 @@ CREATE TABLE IF NOT EXISTS `student` (
   `id` int(11) NOT NULL UNIQUE PRIMARY KEY,
   `student_id` varchar(220) NOT NULL UNIQUE,
   `name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL;
   `report` varchar(2000) NOT NULL,
   `yoa` varchar(45) NOT NULL,
   `parent` varchar(200) NOT NULL,
   `dob` date NOT NULL,
   `gender` varchar(7) NOT NULL,
+  `cgp` varchar(45) NOT NULL,
   `file` varchar(400) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `report`, `yoa`, `parent`, `dob`, `gender`, `file`) VALUES
-(1, 'AS01-2341', 'Ron', 'Weasley', 'hard-working', '2012', '08138652645', '1993-05-05', 'male', 'ronweasley.jpg');
+INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `report`, `yoa`, `parent`, `dob`, `gender`,`cgp`, `file`) VALUES
+(1, 'AS01-2341', 'Ron', 'Weasley', 'hard-working', '2012', '08138652645', '1993-05-05', 'male','9.8', 'ronweasley.jpg');
 
 ALTER TABLE `student`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
