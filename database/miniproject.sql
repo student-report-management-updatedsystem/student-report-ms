e49f8538ad773ff69cd72734c19a47b0509787d6
anjusuresh1 <anjusuresh2001@gmail.com>
2021-05-30 14:10:31 +0400

    database order

--- database/miniproject.sql
+++ database/miniproject.sql
@@ -16,17 +16,17 @@ CREATE TABLE IF NOT EXISTS `student` (
   `student_id` varchar(220) NOT NULL UNIQUE,
   `name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `report` varchar(2000) NOT NULL,
-  `yoa` varchar(45) NOT NULL,
-  `parent` varchar(200) NOT NULL,
   `dob` date NOT NULL,
   `gender` varchar(7) NOT NULL,
+  `parent` varchar(200) NOT NULL,
+  `yoa` varchar(45) NOT NULL,
   `cgpa` float(45) NOT NULL,
+  `report` varchar(2000) NOT NULL,
   `file` varchar(400) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `report`, `yoa`, `parent`, `dob`, `gender`,`cgpa`, `file`) VALUES
-(1, 'AS01-2341', 'Ron', 'Weasley', 'hard-working', '2012', '08138652645', '1993-05-05', 'male','9.8', 'ronweasley.jpg');
+INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `dob`, `gender`, `parent`, `yoa`, `cgpa`,`report`, `file`) VALUES
+(1, 'AS01-2341', 'Ron', 'Weasley', '1993-05-05','male','08138652645', '2012', '9.8','hard-working', 'ronweasley.jpg');
 
 ALTER TABLE `student`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
