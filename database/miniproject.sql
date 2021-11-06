2f2828f8a1aff43370d1982f7894308773ed5f27
anjusuresh1 <anjusuresh2001@gmail.com>
2021-11-06 23:06:45 +0530

    database

--- database/miniproject.sql
+++ database/miniproject.sql
@@ -1,14 +1,35 @@
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(55) NOT NULL,
   `password` varchar(55) NOT NULL
 
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`username`, `password`) VALUES
+INSERT INTO `admin` (`username`, `password`) VALUES
 ('admin', '123');
 
-ALTER TABLE `user`
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(55) NOT NULL,
+  `password` varchar(55) NOT NULL,
+  `role` varchar(55) NOT NULL
+
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`username`, `password`,`role`) VALUES
+('admin', '123','admin');
+INSERT INTO `user` (`username`, `password`,`role`) VALUES
+('usha-01', '05-07-1974','teacher');
+INSERT INTO `user` (`username`, `password`,`role`) VALUES
+('teena-71', '11-09-1972','teacher');
+INSERT INTO `user` (`username`, `password`,`role`) VALUES
+('AS01-2341', '2018-05-05','student');
+INSERT INTO `user` (`username`, `password`,`role`) VALUES
+('AS02-7621', '2017-05-05','student');
+
+ALTER TABLE `admin`
   ADD PRIMARY KEY (`username`);
 
 CREATE TABLE IF NOT EXISTS `kg-1` (
