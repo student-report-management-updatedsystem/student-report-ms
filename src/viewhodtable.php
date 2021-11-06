78df2a02a4aa8a958173d7469c2afbca5870eaad
anjusuresh1 <anjusuresh2001@gmail.com>
2021-11-06 18:18:50 +0530

    hod

--- src/viewhodtable.php
+++ src/viewhodtable.php
@@ -1,132 +1,140 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Panel Page</title>
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    <link rel="stylesheet" href="panel.css">
-  </head>
-  <body>
+<html>
+<head>
+  <title>VIEW TEACHERS</title>
+      <?php include 'tablelinks.php';?>
+      <?php include 'tablestyles.php';?>
+      <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+      <link rel="stylesheet" href="../viewstudent.css">
+</head>
+<body>
+  <div class="sidebar">
+    <div class="sidebar-brand">
+      <h2><span class="lab la-accusoft"></span> ABC School</h2>
+    </div>
 
-      <div class="sidebar">
-        <div class="sidebar-brand">
-          <h2><span class="lab la-accusoft"></span> ABC School</h2>
-        </div>
-
-        <div class="sidebar-menu">
-          <ul>
-            <li>
-              <a href="panel.php" class="active"><span class="las la-school"></span>
-                <span>Dashboard</span></a>
-            </li>
-            <li>
+    <div class="sidebar-menu">
+      <ul>
+        <li>
+          <a href="../panel.php" class="active"><span class="las la-school"></span>
+            <span>Dashboard</span></a>
+        </li>
+        <li>
               <a href="src/chooseclass.php" class="view"><span class="las la-user-graduate"></span>
                 <span>View Students</span></a>
+            </li>
+            <li>
+              <a href="src/addstudent.php"><span class="las la-graduation-cap"></span>
+                <span>Add Students</span></a>
             </li>
             <li>
               <a href="src/viewteachertable.php" class="view"><span class="las la-user-graduate"></span>
                 <span>View Teachers</span></a>
             </li>
             <li>
-              <a href="index.php"><span class="las la-user-minus"></span>
-                <span>Logout</span></a>
+              <a href="src/viewteachertable.php" class="view"><span class="las la-user-graduate"></span>
+                <span>View HOD</span></a>
             </li>
-            <li>
-               <div id="clock">
-                 <div id="time">
-                   <div>
-                     <span id="hour">00</span>
-                     <span>hours</span>
-                   </div>
-                   <div>
-                     <span id="minute">00</span>
-                     <span>minutes</span>
-                   </div>
-                   <div>
-                     <span id="second">00</span>
-                     <span>seconds</span>
-                   </div>
-                 </div>
+        <li>
+          <a href="../index.php"><span class="las la-user-minus"></span>
+            <span>Logout</span></a>
+        </li>
+        <li>
+           <div id="clock">
+             <div id="time">
+               <div>
+                 <span id="hour">00</span>
+                 <span>hours</span>
                </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="main-content">
-         <header>
-           <h2>
-             <label for="">
-               <span class="las la-bars"></span>
-             </label>
-
-             Dashboard
-           </h2>
-
-           <form class="search" action="search.php" method="post" >
-           <div class="search-wrapper">
-           <button type="submit" ><i class="las la-search"></i></button>
-           <input type="text" name="valueToSearch" placeholder="Search Here"required/></br>
-            </div>
-          </form>
-
-           <div class="user-wrapper">
-             <img src="https://img.icons8.com/pastel-glyph/64/000000/user-male--v1.png" width="40px" height="40px"/>
-             <div>
-               <h4>Arya Anju Anjali Adarsh</h4>
-               <small>Super Admin</small>
+               <div>
+                 <span id="minute">00</span>
+                 <span>minutes</span>
+               </div>
+               <div>
+                 <span id="second">00</span>
+                 <span>seconds</span>
+               </div>
              </div>
            </div>
-         </header>
+        </li>
+      </ul>
+    </div>
+  </div>
 
-         <main>
-            <div class="cards">
-              <a href="src/chooseclass.php" style="color:black;"><div class="card-single card-single-1">
-                <div>
-                  <h1>View</h1>
-                  <span>Students</span>
-                </div>
-                <div>
-                  <span class="l-1 las la-users"></span>
-                </div>
-              </div></a>
-              <a href="src/chooseclass.php" style="color:black;"><div class="card-single card-single-1">
-                <div>
-                  <h1>View</h1>
-                  <span>Teachers</span>
-                </div>
-                <div>
-                  <span class="l-1 las la-users"></span>
-                </div>
-              </div></a>
-              <a href="index.php" style="color:black;"><div class="card-single card-single-3">
-                <div>
-                  <h1>Logout</h1>
+  <div class="main-content">
+     <header>
+       <h2>
+         <label for="">
+           <span class="las la-bars"></span>
+         </label>
 
-                </div>
-                <div>
-                  <span class="l-3 las la-user-minus"></span>
-                </div>
-              </div></a>
-            </div>
+         Dashboard
+       </h2>
 
-         </main>
-      </div>
-       <script>
-          var hours = document.getElementById("hour");
-          var minutes = document.getElementById("minute");
-          var seconds = document.getElementById("second");
-          function clock()
-          {
-            var h = new Date().getHours();
-            var m = new Date().getMinutes();
-            var s = new Date().getSeconds();
+       <form class="search" action="../search.php" method="post" >
+       <div class="search-wrapper">
+       <button type="submit" ><i class="las la-search"></i></button>
+       <input type="text" name="valueToSearch" placeholder="Search Here"required/></br>
+        </div>
+      </form>
 
-            hours.innerHTML = h;
-            minutes.innerHTML = m;
-            seconds.innerHTML = s;
-          }
-          var interval = setInterval( clock, 1000);
-       </script>
-  </body>
+       <div class="user-wrapper">
+         <img src="https://img.icons8.com/pastel-glyph/64/000000/user-male--v1.png" width="40px" height="40px"/>
+         <div>
+           <h4>Arya Anju Anjali Adarsh</h4>
+           <small>Super Admin</small>
+         </div>
+       </div>
+     </header>
+     <main>
+       <div class="main-div">
+       <h1 class="student_list">Student List</h1>
+           <table>
+             <thead>
+               <tr>
+                 <th>teacher_id</th>
+                 <th>name</th>
+
+               </tr>
+               <tbody>
+                 <?php
+
+                 include 'auth.php';
+                 $selectquery =" select * from `HOD` ORDER BY name ASC";
+                 $query = mysqli_query($conn,$selectquery);
+
+                   while($res=mysqli_fetch_array($query))
+                   {?>
+                     <tr>
+                         <td><?php echo $res['username']; ?></td>
+                         <td><?php echo $res['name'];?></td>                
+                         </tr>
+
+                 <?php
+                   }
+                 ?>
+               </tbody>
+             </thead>
+
+                 </table>
+       </div>
+     </div>
+     </main>
+     <script>
+        var hours = document.getElementById("hour");
+        var minutes = document.getElementById("minute");
+        var seconds = document.getElementById("second");
+        function clock()
+        {
+          var h = new Date().getHours();
+          var m = new Date().getMinutes();
+          var s = new Date().getSeconds();
+
+          hours.innerHTML = h;
+          minutes.innerHTML = m;
+          seconds.innerHTML = s;
+        }
+        var interval = setInterval( clock, 1000);
+     </script>
+</body>
 </html>
--- src/viewteachertable.php
+++ src/viewteachertable.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>VIEW STUDENT</title>
+  <title>VIEW TEACHERS</title>
       <?php include 'tablelinks.php';?>
       <?php include 'tablestyles.php';?>
       <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
